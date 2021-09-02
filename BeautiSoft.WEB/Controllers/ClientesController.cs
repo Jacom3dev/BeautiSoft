44cbfc6a95d5b6381e3df884a1c701a40b12d17a
jose julian <josejuliandlf@gmail.com>
2021-09-01 23:43:08 -0500

    BeautiSoft

---
+++ BeautiSoft.WEB/Controllers/ClientesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautiSoft.WEB.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult ListarClientes()
+        {
+            @ViewData["Title"] = "Clientes";
+            return View();
+        }
+    }
+}
---
+++ BeautiSoft.WEB/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautiSoft.WEB.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult ListarProductos()
+        {
+            @ViewData["Title"] = "Productos";
+            return View();
+        }
+
+        public IActionResult ListarVentas()
+        {
+            @ViewData["Title"] = "Ventas";
+            return View();
+        }
+
+        public IActionResult ListarCompras()
+        {
+            @ViewData["Title"] = "Compras";
+            return View();
+        }
+
+
+    }
+}
